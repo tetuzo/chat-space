2fbe0bcc9e7b5d465d3037291165d66d7a5a9b3e
tetuzo <s611320@hotmail.co.jp>
2020-02-19 15:49:22 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,51 @@
 $(function(){
   var buildHTML = function(message) {
     if (message.body && message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>`
-        `<div class="upper-message">`
-          `<div class="upper-message__user-name">`
-            message.user_name 
-          `</div>` 
-          `<div class="upper-message__date">` 
-            message.created_at 
-          `</div>` 
-        `</div>` 
-        `<div class="lower-message">` 
-          `<p class="lower-message__content">` 
-            message.body 
-          `</p>` 
-          `<img src="` + message.image + `" class="lower-message__image" >` 
-        `</div>` 
+      var html = `<div class="message" data-message-id=${message.id}>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.body +
+          `</p>` +
+          `<img src= ${message.image} class="lower-message__image" >` +
+        `</div>` +
       `</div>`
     } else if (message.body) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` 
-        `<div class="upper-message">` 
-          `<div class="upper-message__user-name">` 
-            message.user_name 
-          `</div>` 
-          `<div class="upper-message__date">` 
-            message.created_at 
-          `</div>` 
-        `</div>` 
-        `<div class="lower-message">` 
-          `<p class="lower-message__content">` 
-            message.body 
-          `</p>` 
-        `</div>` 
+      var html = `<div class="message" data-message-id=${message.id}>`+
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.body +
+          `</p>` +
+        `</div>` +
       `</div>`
     } else if (message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` 
-        `<div class="upper-message">` 
-          `<div class="upper-message__user-name">` 
-            message.user_name 
-          `</div>` 
-          `<div class="upper-message__date">` 
-            message.created_at 
-          `</div>` 
-        `</div>` 
-        `<div class="lower-message">` 
-          `<img src="` + message.image + `" class="lower-message__image" >` 
-        `</div>` 
+      var html = `<div class="message" data-message-id=${message.id}>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src=${message.image} class="lower-message__image" >` +
+        `</div>` +
       `</div>`
     };
     return html;
@@ -93,7 +93,7 @@ $(function(){
         }
       })
       .fail(function() {
-        console.log('error');
+        alert("自動更新できませんでした。");
      });
    };
    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
